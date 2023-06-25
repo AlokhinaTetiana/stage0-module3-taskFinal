189270f3acfe2c331e70c8d02a528de6c45cb496
AlokhinaTetiana <aliokhinatania03@gmail.com>
2023-06-25 21:35:48 +0300

    k

--- src/main/java/lang/print/gaps/finalModuleTask/DigitsSumCalculator.java
+++ src/main/java/lang/print/gaps/finalModuleTask/DigitsSumCalculator.java
@@ -2,5 +2,10 @@ package lang.print.gaps.finalModuleTask;
 
 public class DigitsSumCalculator {
     public void calculateSum(int number) {
+        int a = number/1000;
+        int b = (number-a*1000)/100;
+        int c = (number%100)%10;
+        int d = number%1000;
+        System.out.println(a+b+c+d);
     }
 }
--- src/main/java/lang/print/gaps/finalModuleTask/NumberReverter.java
+++ src/main/java/lang/print/gaps/finalModuleTask/NumberReverter.java
@@ -2,5 +2,9 @@ package lang.print.gaps.finalModuleTask;
 
 public class NumberReverter {
     public void revert(int number) {
+        int a=number/100;
+        int c = number%10;
+        int b = (number-a*100)/10;
+        System.out.println(a*100+b*10+c);
     }
 }
